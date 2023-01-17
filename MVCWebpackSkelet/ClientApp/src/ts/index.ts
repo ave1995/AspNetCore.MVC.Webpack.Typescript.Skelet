af86a112875cfd805a0ab37a12c475f768d8f9f3
ales.vesely <ales.vesely@dataprojekt.cz>
2023-01-17 11:30:52 +0100

    sample grid

--- MVCWebpackSkelet/ClientApp/src/ts/index.ts
+++ MVCWebpackSkelet/ClientApp/src/ts/index.ts
@@ -1,1 +1,22 @@
-﻿console.log('The \'index\' bundle has been loaded!');
+﻿import {
+    provideFluentDesignSystem,
+    fluentDataGridCell,
+    fluentDataGridRow,
+    fluentDataGrid,
+    DataGrid
+} from "@fluentui/web-components";
+
+provideFluentDesignSystem()
+    .register(
+        fluentDataGridCell(),
+        fluentDataGridRow(),
+        fluentDataGrid()
+    );
+
+let table = document.getElementById("samplegrid") as DataGrid;
+
+table.rowsData = [
+    { item1: "value 1-1", item2: "value 2-1" },
+    { item1: "value 1-2", item2: "value 2-2" },
+    { item1: "value 1-3", item2: "value 2-3" },
+];
